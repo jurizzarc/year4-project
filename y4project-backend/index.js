717e0830b5cb51ae9cec71aede7dc86bf79211f8
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-11-15 00:38:34 +0000

    Set up express and the server

---
+++ y4project-backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+// Set up express
+const app = express();
+app.use(express.json()); // Set up middleware
+app.use(cors());
+
+// Start up the server
+const PORT = process.env.PORT || 8888;
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}.`));
