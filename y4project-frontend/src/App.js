982a4e0a8ee9ed1e6e577f00d423adf05c6a299c
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 18:25:46 +0000

    Apply basic CSS

--- y4project-frontend/src/App.js
+++ y4project-frontend/src/App.js
@@ -2,12 +2,16 @@
 import React from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "./components/pages/Home";
+import Header from "./components/layout/Header";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
+import "./style.css";
 
 export default function App() {
     return <>
         <BrowserRouter>
+            <Header />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/login" component={Login} />
