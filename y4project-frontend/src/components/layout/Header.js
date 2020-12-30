9c9b229b9a9d1f7281dbe57480b332288522f2d1
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 18:29:41 +0000

    Set up header

--- y4project-frontend/src/components/layout/Header.js
+++ y4project-frontend/src/components/layout/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header() {
-    return <div>Header</div>;
+    return (
+        <div>
+            <Link to="/">
+                <h1>Year 4 Project</h1>
+            </Link>
+        </div>
+    );
 }
