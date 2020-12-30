44e64e65d7ebc07795ecf06dd1e3b6fc5189c0ae
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 18:36:48 +0000

    Create AuthOptions component

--- y4project-frontend/src/components/layout/Header.js
+++ y4project-frontend/src/components/layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import AuthOptions from "../auth/AuthOptions";
 
 export default function Header() {
     return (
@@ -7,6 +8,7 @@ export default function Header() {
             <Link to="/">
                 <h1>Year 4 Project</h1>
             </Link>
+            <AuthOptions />
         </div>
     );
 }
