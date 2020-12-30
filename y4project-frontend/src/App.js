4682ee1f1df2c070553106acd6d5543cb1d57ef4
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 23:54:23 +0000

    Implement useState

--- y4project-frontend/src/App.js
+++ y4project-frontend/src/App.js
@@ -1,22 +1,30 @@
 // main component
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Header from "./components/layout/Header";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
 import "./style.css";
 
 export default function App() {
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined
+    });
+
     return <>
         <BrowserRouter>
-            <Header />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </Switch>
+            <UserContext.Provider value={{userData, setUserData}}>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            </UserContext.Provider>
         </BrowserRouter>
     </>;
 }
