256a492a928437f806c29ceee90cb1f848deece4
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 17:59:09 +0000

    Set up react router

--- y4project-frontend/src/App.js
+++ y4project-frontend/src/App.js
@@ -1,6 +1,18 @@
 // main component
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 export default function App() {
-    return <div>App</div>;
+    return <>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        </BrowserRouter>
+    </>;
 }
