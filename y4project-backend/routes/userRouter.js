3d2e5592ae7e0d72a64d9bf778d7912abe1ce440
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-30 16:45:23 +0000

    Modify delete user route

--- y4project-backend/routes/userRouter.js
+++ y4project-backend/routes/userRouter.js
@@ -102,7 +102,12 @@ router.post("/login", async (req, res) => {
 });
 
 router.delete("/delete", auth, async (req, res) => {
-    console.log(req.user);
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.user);
+        res.json(deletedUser);
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 });
 
 module.exports = router;
