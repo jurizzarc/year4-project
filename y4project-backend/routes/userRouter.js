be25b8d6d51b9bfb837c1fe65d2ec170df3ea0de
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2020-12-31 02:04:02 +0000

    Change json response

--- y4project-backend/routes/userRouter.js
+++ y4project-backend/routes/userRouter.js
@@ -137,7 +137,10 @@ router.post("/tokenIsValid", async (req, res) => {
 // endpoint for getting user information
 router.get("/", auth, async (req, res) => {
     const user = await User.findById(req.user);
-    res.json(user);
+    res.json({
+        displayName: user.displayName,
+        id: user._id
+    });
 });
 
 module.exports = router;
