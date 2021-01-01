32d30448889ad9b2143f0bd08029a490b7e55f6e
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-01-01 20:35:45 +0000

    Create register form and register state

--- y4project-frontend/src/components/auth/Register.js
+++ y4project-frontend/src/components/auth/Register.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Register() {
-    return <div>Register</div>;
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [passwordCheck, setPasswordCheck] = useState();
+    const [displayName, setDisplayName] = useState();
+
+    return (
+        <div className="page">
+            <h2>Register</h2>
+            <form>
+                <label htmlFor="register-email">E-mail Address</label>
+                <input 
+                    id="register-email" 
+                    type="email" 
+                    onChange={
+                        (e) => setEmail(e.target.value)
+                    } 
+                />
+
+                <label htmlFor="register-password">Password</label>
+                <input 
+                    id="register-password" 
+                    type="password"
+                    onChange={
+                        (e) => setPassword(e.target.value)
+                    } 
+                />
+                <input 
+                    type="password" 
+                    placeholder="Verify password" 
+                    onChange={
+                        (e) => setPasswordCheck(e.target.value)
+                    } 
+                />
+
+                <label htmlFor="register-display-name">Display Name</label>
+                <input 
+                    id="register-display-name" 
+                    type="text" 
+                    onChange={
+                        (e) => setDisplayName(e.target.value)
+                    } 
+                />
+
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
 }
