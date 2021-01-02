1f2e1274deee5a1556e69b18e7724d866134da41
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-01-02 20:38:56 +0000

    Display error messages on register form

--- y4project-frontend/src/components/auth/Register.js
+++ y4project-frontend/src/components/auth/Register.js
@@ -2,39 +2,50 @@ import React, {useState, useContext} from "react";
 import {useHistory} from "react-router-dom";
 import Axios from "axios";
 import UserContext from "../../context/UserContext";
+import ErrorNotice from "../../components/misc/ErrorNotice";
 
 export default function Register() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [passwordCheck, setPasswordCheck] = useState();
     const [displayName, setDisplayName] = useState();
+    const [error, setError] = useState();
 
     const {setUserData} = useContext(UserContext);
     const history = useHistory();
 
     const submit = async (e) => {
         e.preventDefault();
-        const newUser = {email, password, passwordCheck, displayName};
-        await Axios.post(
-            "http://localhost:8888/users/register",
-            newUser
-        );
-        
-        const loginRes = await Axios.post(
-            "http://localhost:8888/users/login",
-            {email,password}
-        );
-        setUserData({
-            token: loginRes.data.token,
-            user: loginRes.data.user
-        });
-        localStorage.setItem("auth-token", loginRes.data.token);
-        history.push("/");
+
+        try {
+            const newUser = {email, password, passwordCheck, displayName};
+            await Axios.post(
+                "http://localhost:8888/users/register",
+                newUser
+            );
+            
+            const loginRes = await Axios.post(
+                "http://localhost:8888/users/login",
+                {email,password}
+            );
+            setUserData({
+                token: loginRes.data.token,
+                user: loginRes.data.user
+            });
+            localStorage.setItem("auth-token", loginRes.data.token);
+            history.push("/");
+        } catch (err) {
+            err.response.data.msg && setError(err.response.data.msg);
+        }
     };
 
     return (
         <div className="page">
             <h2>Register</h2>
+            {error && (
+                <ErrorNotice message={error} clearError={() => setError(undefined)} />
+            )}
+
             <form className="form" onSubmit={submit}>
                 <label htmlFor="register-email">E-mail Address</label>
                 <input 
