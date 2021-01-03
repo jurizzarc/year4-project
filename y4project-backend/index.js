a7e5a1a7cf60de5182eb8fee3084a8385dba8faf
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-01-03 22:20:19 +0000

    Change port number

--- y4project-backend/index.js
+++ y4project-backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json()); // Set up middleware
 app.use(cors());
 
 // Start up the server
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}.`));
 
 // Set up mongoose
