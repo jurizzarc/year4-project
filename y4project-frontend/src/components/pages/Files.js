952e24192a9584a5671bfb84221c8db6e1ebcb61
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-01-17 03:43:28 +0000

    Uninstall apollo ang graphql packages

--- y4project-frontend/src/components/pages/Files.js
+++ y4project-frontend/src/components/pages/Files.js
@@ -1,12 +1,4 @@
 import React from "react";
-import {useQuery} from "@apollo/react-hooks";
-import gql from "graphql-tag";
-
-export const filesQuery = gql`
-    {
-        files
-    }
-`;
 
 export const Files = () => {
     const {data} = useQuery(filesQuery);
