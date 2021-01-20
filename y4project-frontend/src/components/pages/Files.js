35e9e2bb6a3f32a53cdf52cf1775021ae5494683
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-01-20 00:12:29 +0000

    Create a table for displaying user's uploads in Files.js

--- y4project-frontend/src/components/pages/Files.js
+++ y4project-frontend/src/components/pages/Files.js
@@ -1,11 +1,39 @@
 import React from "react";
 
 export default function Files(props) {
-    console.log(props.uploads);
+    const displayFiles = (props) => {
+        const uploads = props.uploads;
+        
+        if (uploads.length > 0) {
+            return (
+                <table>
+                    <thead>
+                        <tr>
+                            <th>File Name</th>
+                            <th>Detection Type</th>
+                        </tr>
+                    </thead>
+                    {uploads.map((upload) => (
+                        <tbody>
+                            <tr key={upload._id}>
+                                <td>{upload.fileName}</td>
+                                <td>{upload.textDetection}</td>
+                            </tr>
+                        </tbody>
+                    ))}
+                </table>
+            )
+        } else {
+            return (
+                <p>No uploads yet.</p>
+            )
+        }
+    }
 
     return (
         <div>
             <h2>Library</h2>
+            {displayFiles(props)}
         </div>
     );
 }
