a12f8433200064fd3e05883fecd2b2eae5134a22
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-02-04 23:06:06 +0000

    Update Read.js

--- y4project-frontend/src/components/pages/Read.js
+++ y4project-frontend/src/components/pages/Read.js
@@ -2,14 +2,33 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import UserContext from '../../context/UserContext';
 
-export default function Read() {
+export default function Read(props) {
     const { userData } = useContext(UserContext);
+    const [ userUpload, setUserUpload ] = useState();
+    const apiUrl = 'http://localhost:4000/files';
+    const userUploadId = props.match.params.userUploadId;
+    const token = localStorage.getItem('auth-token');
+
+    useEffect(() => {
+        getFileData();
+    }, []);
+
+    const getFileData = () =>{
+        axios.get(`${apiUrl}/read/${userUploadId}`, {
+            headers: {
+                'x-auth-token': token
+            }
+        }).then((response) => {
+            //console.log(response.data);
+            setUserUpload(response.data);
+        }).catch(error => console.error(`Error: ${error}`));
+    };
 
     return (
         <>
             {userData.user ? (
                 <div className="page">
-                    <p>Hello.</p>
+                    <p>{userUpload && userUpload.fileName}</p>
                 </div>
             ) : (
                 <div className="page">
