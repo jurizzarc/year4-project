e23278180862b9cb97117e2b399a31da3894f7e9
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-02-11 01:12:48 +0000

    Update Upload model in upload.js

--- y4project-backend/models/upload.js
+++ y4project-backend/models/upload.js
@@ -4,7 +4,8 @@ const uploadSchema = new mongoose.Schema({
     fileName: { type: String, required: true },
     publicUrl: { type: String, required: true },
     textDetection: { type: String, required: true },
-    userId: { type: String, required: true }
+    userId: { type: String, required: true },
+    detections: [{ text: String }]
 });
 
 module.exports = Upload = mongoose.model('upload', uploadSchema);
