5931861650726a06dbbba62409d1a310603ea305
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-02-11 03:34:33 +0000

    Update Read.js

--- y4project-frontend/src/components/pages/Read.js
+++ y4project-frontend/src/components/pages/Read.js
@@ -1,34 +1,48 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import UserContext from '../../context/UserContext';
+// import NewlineText from '../misc/NewlineText';
 
 export default function Read(props) {
     const { userData } = useContext(UserContext);
     const [ userUpload, setUserUpload ] = useState();
+    const [ detections, setDetections ] = useState();
     const apiUrl = 'http://localhost:4000/uploads';
-    const userUploadId = props.match.params.userUploadId;
-    const token = localStorage.getItem('auth-token');
 
     useEffect(() => {
+        const userUploadId = props.match.params.userUploadId;
+        const token = localStorage.getItem('auth-token');
+
+        const getFileData = () => {
+            axios.get(`${apiUrl}/read/${userUploadId}`, {
+                headers: {
+                    'x-auth-token': token
+                }
+            }).then((response) => {
+                setUserUpload(response.data);
+                setDetections(response.data.detections);
+            }).catch(error => console.error(`Error: ${error}`));
+        };
+
         getFileData();
     }, []);
 
-    const getFileData = () =>{
-        axios.get(`${apiUrl}/read/${userUploadId}`, {
-            headers: {
-                'x-auth-token': token
-            }
-        }).then((response) => {
-            //console.log(response.data);
-            setUserUpload(response.data);
-        }).catch(error => console.error(`Error: ${error}`));
-    };
+    const trim = (str) => {
+        return str.toString()
+                  .replace(/^"(.+(?="$))"$/, '$1')
+    }
 
     return (
         <>
             {userData.user ? (
                 <div className="page">
-                    <p>{userUpload && userUpload.fileName}</p>
+                    <section className="text">
+                        {detections && detections.map(detection => (
+                            <p key={detection._id}>
+                                {trim(detection.text)}
+                            </p>
+                        ))}
+                    </section>
                 </div>
             ) : (
                 <div className="page">
