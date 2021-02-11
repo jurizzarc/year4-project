a496b6b90342aabc1091ac83347f514901003dc2
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-02-11 15:58:23 +0000

    Remove publicUrl property from uploadSchema in upload.js

--- y4project-backend/models/upload.js
+++ y4project-backend/models/upload.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const uploadSchema = new mongoose.Schema({
     fileName: { type: String, required: true },
-    publicUrl: { type: String, required: true },
     textDetection: { type: String, required: true },
     userId: { type: String, required: true },
     detections: [{ text: String }]
