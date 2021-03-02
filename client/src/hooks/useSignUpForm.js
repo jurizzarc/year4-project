756a5eb6bfc7ff4d5ec17ec216549662a93360a9
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-03-02 04:28:55 +0000

    Update useSignUpForm hook

--- client/src/hooks/useSignUpForm.js
+++ client/src/hooks/useSignUpForm.js
@@ -1,17 +1,49 @@
 import { useState, useEffect } from 'react';
-import UserContext from '../contexts/UserContext';
-import axios from 'axios';
 
-const useSignUpForm = validateUserInfo => {
-    const [values, setValues] = useState({
-        displayName: '',
-        email: '',
-        password: '',
-        passwordCheck: ''
-    });
+const useSignUpForm = (initialState, validate, runOnSubmit) => {
+    const [values, setValues] = useState(initialState);
     const [errors, setErrors] = useState({});
+    // touched is an array of all fields that have been touched by the user
+    const [touched, setTouched] = useState([]);
+    const [isSubmitting, setSubmitting] = useState(false);
 
-    const handleChange = e => {
+    // Runs on component load and every time the errors object changes
+    useEffect(() => {
+        // Call form submission if submit button was hit
+        if (isSubmitting) {
+            const noErrors = Object.keys(errors).length === 0;
+            // Call form submission only if there are no errors
+            if (noErrors) {
+                // Clear touched array
+                setTouched([]);
+                // Run form submission
+                runOnSubmit();
+                setSubmitting(false);
+            } else {
+                setSubmitting(false);
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [errors]);
+
+    // Reruns after there is a change to touched array or values
+    // Checks to see if there are any errors
+    useEffect(() => {
+        const validationErrors = validate(values);
+        const touchedErrors = Object.keys(validationErrors)
+            .filter(key => touched.includes(key)) // Get all touched keys
+            .reduce((acc, key) => {
+                if (!acc[key]) {
+                    acc[key] = validationErrors[key];
+                }
+                return acc;
+            }, {});
+            setErrors(touchedErrors);
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [touched, values]);
+
+    // Runs every time an input changes
+    const handleChange = (e) => {
         const { name, value } = e.target;
         setValues({
             ...values,
@@ -19,12 +51,34 @@ const useSignUpForm = validateUserInfo => {
         });
     };
 
-    const handleSubmit = e => {
+    // Runs when a field is tapped into
+    const handleBlur = (e) => {
+        // If field hasn't been touched before add it to the array
+        if (!touched.includes(e.target.name)) {
+            setTouched([
+                ...touched,
+                e.target.name
+            ]);
+        }
+    };
+
+    // Runs on form submission
+    const handleSubmit = (e) => {
         e.preventDefault();
-        setErrors(validateUserInfo(values));
+        const validationErrors = validate(values);
+        setErrors(validationErrors);
+        setSubmitting(true);
     }
 
-    return { handleChange, values, handleSubmit, errors };
+    // Return values from hook to be used in the form component
+    return { 
+        handleSubmit,
+        handleBlur,
+        handleChange,
+        values,
+        errors,
+        isSubmitting
+    };
 };
 
 export default useSignUpForm;
