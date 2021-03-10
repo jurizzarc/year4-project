8d40de16f67cd9f24e453f750af850e9e0e12da0
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-03-10 22:23:46 +0000

    Add keyboard listeners to Modal component

--- client/src/components/elements/Modal.js
+++ client/src/components/elements/Modal.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useEffect, createRef } from 'react';
 import { createPortal } from 'react-dom';
 
 const ModalContext = createContext();
@@ -17,6 +17,40 @@ export default function Modal({
     const checkModalSize = SIZES.includes(modalSize)
         ? modalSize
         : SIZES[0];
+    
+    // Keyboard shortcut
+    useEffect(() => {
+        function keyListener(e) {
+            const listener = keyListenersMap.get(e.keyCode);
+            return listener && listener(e);
+        };
+
+        document.addEventListener('keydown', keyListener);
+
+        return () => document.removeEventListener('keydown', keyListener);
+    });
+
+    const modalRef = createRef();
+    const handleTabKey = (e) => {
+        const focusableModalElements = modalRef.current.querySelectorAll(
+            'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+        );
+        const firstElement = focusableModalElements[0];
+        const lastElement = focusableModalElements[focusableModalElements.length - 1];
+
+        if (!e.shiftKey && document.activeElement !== firstElement) {
+            firstElement.focus();
+            return e.preventDefault();
+        }
+
+        if (e.shiftKey && document.activeElement !== lastElement) {
+            lastElement.focus();
+            e.preventDefault();
+        }
+    };
+
+    const keyListenersMap = new Map([[27, onModalClose], [9, handleTabKey]]);
+    
     return createPortal(
         <>
             <div
