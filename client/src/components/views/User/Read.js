6fa0ca0a4c11ead8a39c433d65de3c30dae49cfe
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-03-18 03:40:00 +0000

    Update Read component

--- client/src/components/views/User/Read.js
+++ client/src/components/views/User/Read.js
@@ -1,57 +1,40 @@
-import React, { useState, useEffect, useContext, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import UserContext from '../../../contexts/UserContext';
+import ReadingSettings from '../../elements/ReadingSettings';
+import NewLineText from '../../elements/NewLineText';
 
-export default function Read(props) {
-    const { userData } = useContext(UserContext);
-    const [ userUpload, setUserUpload ] = useState({});
-    const [ detections, setDetections ] = useState([]);
-    const apiUrl = 'http://localhost:4000/uploads';
+const Read = (props) => {
+    const BASE_API_URL = 'http://localhost:4000/uploads';
+    const [userUpload, setUserUpload] = useState({});
+    const [detectedText, setDetectedText] = useState([]);
 
     useEffect(() => {
         const userUploadId = props.match.params.userUploadId;
         const token = localStorage.getItem('auth-token');
 
         const getFileData = () => {
-            axios.get(`${apiUrl}/read/${userUploadId}`, {
+            axios.get(`${BASE_API_URL}/read/${userUploadId}`, {
                 headers: {
                     'x-auth-token': token
                 }
             }).then((response) => {
                 setUserUpload(response.data);
-                setDetections(response.data.detections);
+                setDetectedText(response.data.detections);
             }).catch(error => console.error(`Error: ${error}`));
         };
-
         getFileData();
     }, []);
 
-    let detectionsArray, extractedText, newText;
-    if (detections) {
-        detectionsArray = detections.map(detection => detection.text);
-        extractedText = detectionsArray.join('');
-        newText = extractedText.split('\n').map((value, index) => {
-            return (
-                <p key={index}>
-                    {value}
-                </p>
-            )
-        });
-    }
-
     return (
-        <>
-            {userData.user ? (
-                <div className="page">
-                    <section className="text">
-                        {newText}
-                    </section>
-                </div>
-            ) : (
-                <div className="page">
-                    Access not allowed.
-                </div>
-            )}
-        </>
+        <main className="reading-container">
+            <ReadingSettings />
+            <section className="article-container">
+                <article>
+                    {detectedText && <NewLineText detections={detectedText} />}
+                </article>
+            </section>
+        </main>
     )
 }
+
+export default Read;
