cd51e63988980c32c96f7ae9c12178da3c042e49
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-03-19 03:26:28 +0000

    Convert string to markdown if text is handwritten in NewLineText.js

--- client/src/components/elements/NewLineText.js
+++ client/src/components/elements/NewLineText.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import { Markup } from 'interweave';
 
 const NewLineText = (props) => {
+    const detectionType = props.detectionType;
     const detections = props.detections;
     const detectionsArray = detections.map(detection => detection.text);
-    const extractedText = detectionsArray.join('');
-    const newText = extractedText.split('\n').map((value, index) => {
-        return (
-            <p key={index}>
-                {value}
-            </p>
-        )
-    });
+    let extractedText = detectionsArray.join('');
 
-    return newText;
+    if (detectionType !== 'hndwrtng-img') {
+        const newText = extractedText.split('\n').map((value, index) => {
+            return (
+                <p key={index}>
+                    {value}
+                </p>
+            )
+        });
+
+        return newText;
+    } else {
+        // Pre Clean
+        let cleanText = extractedText;
+        ['[h]', '[s]', '[p]', '[b]', '[i]'].forEach((item, i) => {
+            cleanText = cleanText.split(item).join('\n' + item.toUpperCase());
+            cleanText = cleanText.split(item.toUpperCase()).join('\n' + item.toUpperCase());
+        });
+        cleanText = cleanText.split('\n\n').join('\n');
+
+        let lines = cleanText.split('\n');
+        let resultHtml = '';
+        lines.forEach((line, i) => {
+            let temp = line.substring(3);
+            if (line.toUpperCase().includes('[H]')) {
+                resultHtml = resultHtml + '<h1>' + temp + '</h1>';
+            } else if (line.toUpperCase().includes('[S]')) {
+                resultHtml = resultHtml + '<h2>' + temp + '</h2>';
+            } else if (line.toUpperCase().includes('[P]')) {
+                resultHtml = resultHtml + '<p>' + temp + '</p>';
+            } else if (line.toUpperCase().includes('[B]')) {
+                resultHtml = resultHtml + '<p><strong>' + temp + '</strong></p>';
+            } else if (line.toUpperCase().includes('[I]')) {
+                resultHtml = resultHtml + '<p><em>' + temp + '</em></p>'
+            }
+        });
+        // resultHtml = resultHtml + '</>';
+
+        // Post Clean
+        // let tags = ['<h1>', '<h2>', '<p>', '<strong>', '<em>'];
+        // tags.forEach((tag, i) => {
+        //     resultHtml = resultHtml.split(tag + '').join(tag);
+        //     let closingTag = tag.split('<').join('</');
+        //     resultHtml = resultHtml.split(' ' + closingTag).join(closingTag);
+        // });
+        // console.log(typeof resultHtml);
+        console.log(resultHtml);
+        return <Markup content={resultHtml} />;
+    }
 }
 
 export default NewLineText;
