d4b381a55e6ee7951f6b60f8d8d20baa6afc7040
Jurizza Cuaresma <jurizzacuaresma@gmail.com>
2021-03-20 02:20:57 +0000

    Update src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,23 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const userRoutes = require('../routes/user-routes');
 const uploadRoutes = require('../routes/upload-routes');
 
-// Set up express
+// Set up the server
 const app = express();
-// Set up middleware
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}.`));
+
+// Parse JSON
 app.use(express.json()); 
 app.use(cors());
 // Transpile the request into something that mongo will understand
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-// Start up the server
-const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}.`));
 
 // Connect to MongoDB
 mongoose.Promise = global.Promise;
@@ -27,10 +27,10 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     useUnifiedTopology: true,
     useCreateIndex: true
 }, (err) => {
-    if (err) throw err;
+    if (err) return console.error(err);
     console.log('MongoDB connection established.');
 });
 
-// Routes
+// Set up routes
 app.use('/users', userRoutes);
 app.use('/uploads', uploadRoutes);
